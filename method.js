e6074fef7d5615d1169f6ba91dd12936ef3471c4
vue <451476398@qq.com>
2019-04-11 11:46:16 +0800

    Update method.js

--- method.js
+++ method.js
@@ -61,6 +61,21 @@ Array.prototype.remove = function(obj){
     }
   }
 }
+// 对象数组去重 根据对象某一属性判断是否重复
+function unique(objArray, attrName) {
+  var result = [];//去重后返回的结果数组
+  var temp = {};//临时对象
+  //将对象数组中每一项的name值作为属性，若temp不拥有此属性时则为temp添加此属性且将其值赋为true，并将这一项push到结果数组中
+  for(var i = 0; i < objArray.length; i++){  
+      var myAttr = objArray[i][attrName];
+      if(temp[myAttr]){//如果temp中已经存在此属性名，则说明遇到重复项
+          continue;//不继续执行接下来的代码，跳转至循环开头
+      }  
+      temp[myAttr] = true;//为temp添加此属性（myname）且将其值赋为true
+      result.push(objArray[i]);//将这一项复制到结果数组result中去
+  }  
+  return result;  
+}
 //数组去重
  Array.prototype.unique1 = function(){
      var res = [this[0]];
